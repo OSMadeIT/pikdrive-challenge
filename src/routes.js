02e81aad785884dc9780a253c0c9950e5b2bd004
O S <osmadeit@gmail.com>
2021-04-24 20:24:00 +0300

    add product suppliers routes

--- src/routes.js
+++ src/routes.js
@@ -2,10 +2,12 @@ import orders from './components/orders/routes';
 import suppliers from './components/suppliers/routes';
 import topsales from './components/top-sales/routes';
 import products from './components/products/routes';
+import productSuppliers from './components/product-suppliers/routes';
 
 export default [
   ...orders,
   ...suppliers,
   ...topsales,
   ...products,
+  ...productSuppliers,
 ];
