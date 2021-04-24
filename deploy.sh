5f95c98f19aa9c2f50933808e7eeb1d04a2bf9b1
O S <osmadeit@gmail.com>
2021-04-24 21:32:30 +0300

    correct deploy script typos

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env sh
 
-# abort on errorsset -e
-# buildyarn run build
-# navigate into the build output directorycd dist
+# abort on errors
+set -e
+# build
+yarn run build
+# navigate into the build output directory
+cd dist
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
 
@@ -10,3 +13,4 @@ git init
 git add -A
 git commit -m 'deploy pikdrive challenge'
 git push -f https://github.com/OSMadeIT/pikdrive-challenge.git master:gh-pages
+cd -
